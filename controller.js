bf93c05110269709ec3b1a7de235f0c84b663179
shadow-island <uudoin@gmail.com>
2016-10-29 20:24:35 -0600

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,7 @@
+function StudyMemberListCtrl($scope){
+$scope.members = [
+{"name":"C#","message":"I love C#"},
+{"name":"Grails","message":"I love Grails"},
+{"name":"Grandle","message":"I love Grandle"}
+];
+}
